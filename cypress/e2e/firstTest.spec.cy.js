13275e3869285999a3f254652da69b81dc7401a6
darina.Ilenko <dilenko13@gmail.com>
2022-07-22 17:13:15 +0300

    add comment

--- cypress/e2e/firstTest.spec.cy.js
+++ cypress/e2e/firstTest.spec.cy.js
@@ -40,15 +40,14 @@
 //   cy.visit('https://next.privat24.ua/')
 //   cy.get('*[class^="card"]')
 // });
-
-
-it.only('Using Get with Find and Eq', () => {
-  cy.visit('https://next.privat24.ua/deposit/open/')
-  cy.get('tbody').find('td').find('div').find('button').eq('0')
+ //Comment
+it.only("Using Get with Find and Eq", () => {
+  cy.visit("https://next.privat24.ua/deposit/open/");
+  cy.get("tbody").find("td").find("div").find("button").eq("0");
 });
 
-it.only('Using Get with Find and Eq', () => {
-  cy.viewport(1800, 700)
-  cy.visit('https://docs.cypress.io/api/commands/eq            .html#Syntax')
-  cy.get('aside').find('div').find('ul').find('li').find('a').eq(0)
+it.only("Using Get with Find and Eq", () => {
+  cy.viewport(1800, 700);
+  cy.visit("https://docs.cypress.io/api/commands/eq.html#Syntax");
+  cy.get("aside").find("div").find("ul").find("li").find("a").eq(0);
 });
